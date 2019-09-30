f09f5be8dd0572bac0f474e0e1764ee9dd0ea09f
Olivier Hendriks <olivier.hendriks@student.hu.nl>
2019-09-30 15:55:54 +0200

    Dependencies van Angular/Firebase en de initialisatie van de Firebase app toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import {
   MatCardModule
 } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirebaseConfiguration } from './config/firebase-configuration';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    /* Firebase modules */
+    AngularFireModule.initializeApp(FirebaseConfiguration),
+    AngularFirestoreModule,
 
     /* Material modules */
     MatCardModule,
