55083c3cb9588290de1cb463be846e82b95037b8
Olivier Hendriks <olivier.hendriks@student.hu.nl>
2019-09-30 15:02:45 +0200

    Aanvraag model toegevoegd

---
+++ src/app/models/aanvraag-status.ts
@@ -0,0 +1,5 @@
+enum AanvraagStatus {
+    REQUESTED,
+    ACCEPTED,
+    REJECTED
+}
---
+++ src/app/models/aanvraag.ts
@@ -0,0 +1,14 @@
+interface Aanvraag {
+    aanvraagId?: string;
+    docentId?: string;
+    aanvragerId: string;
+    ruimteId: string;
+    qrCode?: string;
+    startTijd: string;
+    eindTijd: string;
+    motivatie: string;
+    status: {
+        aanvraagStatus: AanvraagStatus,
+        toelichting?: string
+    };
+}
