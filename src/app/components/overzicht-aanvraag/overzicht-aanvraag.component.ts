b184a9b62318cc5d7ba3b63a78e106228adb28c6
Olivier Hendriks <olivier.hendriks@student.hu.nl>
2019-10-09 11:19:59 +0200

    Mock-components voor tijdelijke referenties

---
+++ src/app/components/overzicht-aanvraag/overzicht-aanvraag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overzicht-aanvraag',
+  templateUrl: './overzicht-aanvraag.component.html',
+  styleUrls: ['./overzicht-aanvraag.component.scss']
+})
+export class OverzichtAanvraagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
