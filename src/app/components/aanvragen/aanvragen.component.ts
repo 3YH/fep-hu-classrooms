4d035bd179486037d5e98f3bd05baf028659be0f
Yannick <yannick@netc.eu>
2019-10-10 16:56:25 +0200

    Content filter voor alle rollen
    Gebruik *ngIf="isRolnaam"

--- src/app/components/aanvragen/aanvragen.component.ts
+++ src/app/components/aanvragen/aanvragen.component.ts
@@ -4,19 +4,19 @@ import {
   OnDestroy,
   OnInit,
   ViewChild
-} from '@angular/core';
-import { MatChip } from '@angular/material';
-import { MatDialog } from '@angular/material/dialog';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { WhereClause } from '../../models/where-clause';
-import { AanvraagService } from '../../services/aanvraag.service';
-import { ExampleDialogComponent } from '../example-dialog/example-dialog.component';
-import { AuthenticationService } from 'src/app/services/authentication.service';
-import { User } from 'src/app/models/user';
+} from "@angular/core";
+import { MatChip } from "@angular/material";
+import { MatDialog } from "@angular/material/dialog";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import { WhereClause } from "../../models/where-clause";
+import { AanvraagService } from "../../services/aanvraag.service";
+import { ExampleDialogComponent } from "../example-dialog/example-dialog.component";
+import { AuthenticationService } from "src/app/services/authentication.service";
+import { User } from "src/app/models/user";
 
 @Component({
   selector: "app-aanvragen",
@@ -36,7 +36,9 @@ export class AanvragenComponent implements AfterViewInit, OnInit, OnDestroy {
   ];
   public isLoading = true;
   public members;
+  public isStudent = false;
   public isDocent = false;
+  public isScanner = false;
 
   @ViewChild(MatPaginator, { static: false }) private paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) private sort: MatSort;
@@ -44,21 +46,23 @@ export class AanvragenComponent implements AfterViewInit, OnInit, OnDestroy {
   constructor(
     private authenticationService: AuthenticationService,
     private aanvraagService: AanvraagService,
-    public dialog: MatDialog,
+    public dialog: MatDialog
   ) {}
 
   public ngOnInit() {
-    this.hasDocentRole();
-    this.getFilteredData('REQUESTED');
+    this.hasRole();
+    this.getFilteredData("REQUESTED");
   }
 
-  public hasDocentRole() {
-        return this.authenticationService.getCurrentUserInfo().subscribe((user: User) => {
-          // if(user.role === 'docent'){ this.isDocent = true;}
-          user.role === 'docent' && (this.isDocent = true);
-          });
-    }
-
+  public hasRole() {
+    return this.authenticationService
+      .getCurrentUserInfo()
+      .subscribe((user: User) => {
+        user.role === "student" && (this.isStudent = true);
+        user.role === "docent" && (this.isDocent = true);
+        user.role === "scanner" && (this.isScanner = true);
+      });
+  }
 
   public filterByStatus(chip: MatChip, status: string) {
     chip.toggleSelected();
